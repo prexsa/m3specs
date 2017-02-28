ce5a6542dfd8f2fea29eea0d7ea9d59c024978f6
prexsa <prexgit@gmail.com>
2017-02-27 20:10:48 -0800

    [progress] styling on car detail page

--- src/components/e30.js
+++ src/components/e30.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Button, Header, List } from 'semantic-ui-react';
+import { Button, Divider, Header, Image, Item, List, Menu } from 'semantic-ui-react';
+
+import Navigation from '../components/Navigation';
+import GearRatio from '../components/GearRatio';
 
 export default class e30 extends Component {
   render() {
+    var style = {
+      opacity: 0.85
+    }
+
     return (
       <div className='car-details-container'>
-        <Header as="h1" size='huge'>E30 M3</Header>
-        <p>Find Local Listings</p>
-        <Button basic color='green'>Search</Button>
+        <Navigation />
+        <div className='banner-container'>
+          <Image  style={style} fluid src="../../assets/e30.jpg" />
+          <div className='banner'>
+            <Header as="h1" size='huge' color='grey' textAlign='center'>E30 M3</Header>
+            <Header as="h3" size='large' color='grey' textAlign='center'>Find Local Listings</Header>
+            <Button basic color='green'>Search</Button>
+          </div>
+        </div>
+
+        <Divider hidden={true} />
 
         <div>
-          <Header as='h2' size='large'>OVERVIEW</Header>
+          <Button.Group widths='5' basic>
+            <Button>OverView</Button>
+            <Button>Gear Ratio Calculations</Button>
+            <Button>Press & Reviews</Button>
+            <Button>For Sale</Button>
+            <Button>Local Meetups</Button>
+          </Button.Group>
+          <Header as='h2' size='large' color='blue' textAlign='center'>OVERVIEW</Header>
           <List>
             <List.Item>Body Style: 2-door Coupe / Convertible</List.Item>
             <List.Item>Engine: 2.3L Inline 4</List.Item>
@@ -21,7 +43,33 @@ export default class e30 extends Component {
             <List.Item>Curb Weight: 2,568.4 - 2,998.3 lb</List.Item>
           </List>
 
-          <Header as='h2' size='large'>Gear Ratio Calculations</Header>
+          <GearRatio />
+
+          <Header as='h2' size='large' color='blue' textAlign='center'>Press & Reviews</Header>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Item.Header as='a'>Header</Item.Header>
+                <Item.Meta>Description</Item.Meta>
+                <Item.Description>
+                  <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+                </Item.Description>
+                <Item.Extra>Additional Details</Item.Extra>
+              </Item.Content>
+            </Item>
+            <Item>
+              <Item.Content>
+                <Item.Header as='a'>Header</Item.Header>
+                <Item.Meta>Description</Item.Meta>
+                <Item.Description>
+                  <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+                </Item.Description>
+                <Item.Extra>Additional Details</Item.Extra>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+          <Header as='h2' size='large' color='blue' textAlign='center'>For Sale</Header>
+          <Header as='h2' size='large' color='blue' textAlign='center'>Local Meetups</Header>
         </div>
 
       </div>
